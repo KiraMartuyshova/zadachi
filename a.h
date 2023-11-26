a958879a566c0bac474dd90e1e9b1e7075fad79f
KiraMartyushova <152027084+KiraMartuyshova@users.noreply.github.com>
2023-11-26 12:03:45 +0300

    Update a.h

--- a.h
+++ a.h
@@ -1,18 +1,18 @@
 #pragma once 
 #include <iostream>
 
-//Êëàññ ÷èñåë ñ àáñîëþòíîé ïîãðåøíîñòüþ
+//Класс чисел с абсолютной погрешностью
 
 class abs_error 
 {
 public:
-	//Êîíñòðóêòîðû
-	abs_error(); //Êîíñòðóêòîð ïî óìîë÷àíèþ
+	//Конструкторы
+	abs_error(); //Конструктор по умолчанию
 	abs_error(double x);
 	abs_error(double x, double y);
-	abs_error(const abs_error& other); //Êîíñòðóêòîð êîïèðîâàíèÿ
+	abs_error(const abs_error& other); //Конструктор копирования
 	
-	//Ïåðåãðóæåííûå îïåðàòîðû àðèôìåòè÷åñêèõ îïåðàöèé
+	//Перегруженные операторы арифметических операций
 	abs_error operator=(const abs_error& other); 
 	abs_error operator+=(const abs_error& other); 
 	abs_error operator/=(const abs_error& other); 
@@ -23,19 +23,19 @@ public:
 	abs_error operator*(const abs_error& other) const;
 	abs_error operator/(const abs_error& other) const;
 	
-	//Ïåðåãðóæåííûé îïåðàòîð ðàâåíñòâà äëÿ ñðàâíåíèÿ äâóõ îáúåêòîâ
+	//Перегруженный оператор равенства для сравнения двух объектов
 	bool operator==(const abs_error& other) const; 
 	
-	//Ïåðåãðóæåííûé îïåðàòîð <<, ïîçâîëÿþùèé âûâîäèòü îáúåêòû êëàññà â ïîòîê 
+	//Перегруженный оператор <<, позволяющий выводить объекты класса в поток 
 	friend std::ostream& operator<<(std::ostream& os, const abs_error& num); 
 
-	//Ìåòîäû ïîëó÷åíèÿ è óñòàíîâêè çíà÷åíèé
+	//Методы получения и установки значений
 	double Get_Num() const;			
 	double Get_Err() const;
 	void Set_Num(double x);
 	void Set_Err(double x);
 
-	//Ìåòîäû âûâîäà
+	//Методы вывода
 	void Print_Num();
 	void Print_Err();
 	void Print();
